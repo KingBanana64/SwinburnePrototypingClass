59ce940722d5c74585678a341dce850e685afb0b
KingBanana64 <126744653+KingBanana64@users.noreply.github.com>
2025-08-25 23:05:49 +1000

    tweaks

--- gamefile/RhythmTest2/inputHandler.gd
+++ gamefile/RhythmTest2/inputHandler.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
+@onready var song = $Song
+@onready var timer = $SongLength
+
 func _process(delta: float) -> void:
-	pass
+	print(timer.time_left)
 
 ## When song finishes
 func _on_song_length_timeout() -> void:
--- gamefile/RhythmTest2/level_editor.gd
+++ gamefile/RhythmTest2/level_editor.gd
@@ -11,7 +11,6 @@ var levelInfo = {
 }
 
 func _ready() -> void:
-	
 	if editMode:
 		pass
 	else:
