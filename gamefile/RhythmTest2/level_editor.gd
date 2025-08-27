979c90f8a07c6e9c12b752f014a31ccc0fc512ae
KingBanana64 <126744653+KingBanana64@users.noreply.github.com>
2025-08-27 14:29:46 +1000

    Polish & begining to add multiple animal inputs

--- gamefile/RhythmTest2/level_editor.gd
+++ gamefile/RhythmTest2/level_editor.gd
@@ -1,21 +1,27 @@
 extends Node2D
 
 ## Level Making, manually change to write Levels
-var editMode = false
+var editMode = true
 ## left bracket is for the animal, right is for the player
 var EDIT_tap_times = [[],[]]
 var current_level_name = "McDInThePentagon_short"
 
 ## !!!FIX!!BUG!!! Name of node wont always be Test1
-@onready var input_handler = get_node("/root/Test1")
+@onready var input_handler = get_node("/root/level01")
 
 ## FOR "tap_times" - left bracket is for the animal, right is for the player
 var levelInfo = {
 	"McDInThePentagon" = {
-		"tap_times": "[[1,2,3,4],[5,6,7,8]]"
+		"tap_times": "[[1,2,3,4],[5,6,7,8]]",
+		"animal_amount": 1
 	},
-	"McDInThePentagon_short" = {
-		"tap_times": "[[1.03194444444447, 1.43144434920638, 1.81813055555559, 2.17229722222227, 3.88115222222231, 4.25615222222232, 4.61726400000011, 4.95079574603185], [2.53372563492069, 2.88800088888895, 3.24216755555563, 3.56161200000008, 5.28412907936518, 5.62440685714296, 5.95774019047629, 6.29801796825407]]"
+	#"McDInThePentagon_short" = {
+		#"tap_times": "[[1.05102644444447, 1.44021611111115, 1.80132722222227, 2.15549388888894, 3.88470622222233, 4.27359511111123, 4.60692844444456, 4.94720622222234], [2.50271611111118, 2.82910500000007, 3.17637288888897, 3.53748400000009, 5.26666666666678, 5.61388888888901, 5.93333333333345, 6.28750000000012]]",
+		#"animal_amount": 1
+	#},
+		"McDInThePentagon_short" = {
+		"tap_times": "[[1,2,3,4],[5,6,7,8]]",
+		"animal_amount": 2
 	},
 }
 
@@ -24,17 +30,34 @@ func _ready() -> void:
 	if !editMode:
 		var tap_times = levelInfo.get(current_level_name).get("tap_times")
 		var tap_times_arr = str_to_var(tap_times)
-		input_handler.organise_inputs(tap_times_arr)
+		var animal_amount = levelInfo.get(current_level_name).get("animal_amount")
+		input_handler.organise_inputs(tap_times_arr, animal_amount)
 
 
 func _process(delta: float) -> void:
 	if editMode:
-		if Input.is_action_just_pressed("tap"):
-			EDIT_tap_times[1].append(input_handler.time_passed)
-			pass
-		elif Input.is_action_just_pressed("EDIT_Add"):
+		if Input.is_action_just_pressed("EDIT_Bark1"):
 			EDIT_tap_times[0].append(input_handler.time_passed)
-			pass
+		elif Input.is_action_just_pressed("EDIT_Tap1"):
+			EDIT_tap_times[1].append(input_handler.time_passed)
+		
+		elif Input.is_action_just_pressed("EDIT_Bark2"):
+			## Check if array size exists, then add it
+			if EDIT_tap_times.size() <= 2:
+				EDIT_tap_times.append_array([[],[]])
+			
+			EDIT_tap_times[2].append(input_handler.time_passed)
+		elif Input.is_action_just_pressed("EDIT_Tap2"):
+			EDIT_tap_times[3].append(input_handler.time_passed)
+		
+		elif Input.is_action_just_pressed("EDIT_Bark2"):
+			## Check if array size exists, then add it
+			if EDIT_tap_times.size() <= 4:
+				EDIT_tap_times.append_array([[],[]])
+			
+			EDIT_tap_times[4].append(input_handler.time_passed)
+		elif Input.is_action_just_pressed("EDIT_Tap2"):
+			EDIT_tap_times[5].append(input_handler.time_passed)
 
 
 func finish():
