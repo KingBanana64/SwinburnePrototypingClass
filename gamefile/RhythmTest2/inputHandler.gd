cce55c03cd0e764b9119b813312f742049e0e5e4
ChristopherBrand03 <126744653+KingBanana64@users.noreply.github.com>
2025-09-01 10:42:53 +1000

    Revert "Polish & begining to add multiple animal inputs"
    This reverts commit 979c90f8a07c6e9c12b752f014a31ccc0fc512ae.

--- gamefile/RhythmTest2/inputHandler.gd
+++ gamefile/RhythmTest2/inputHandler.gd
@@ -7,38 +7,35 @@ extends Node2D
 ## delay of hitting buttons on time
 var delay = 0.2
 var time_passed
-var animal_queue1 = []
-var animal_queue2 = []
-var animal_queue3 = []
-var input_queue1 = []
-var input_queue2 = []
-var input_queue3 = []
+var animal_queue = []
+var input_queue = []
+
 
 func _process(delta: float) -> void:
 	
 	time_passed = timer.wait_time - timer.time_left
 	
 	## ----------------- INPUTS -----------------
-	if input_queue1.size() > 0: 
+	if input_queue.size() > 0: 
 		## If next input has passed current time, pop and indicate fail
-		if input_queue1.front() < (time_passed - delay):
-			input_queue1.pop_front()
+		if input_queue.front() < (time_passed - delay):
+			input_queue.pop_front()
 			print("KEY PASSED")
 		
 		## If Input pressed and input is close enough to current time, pop & indicate sucsess
 		if Input.is_action_just_pressed("tap"):
 			## !!FIX!! add animal collisions here
-			if input_queue1.front() < (time_passed + delay):
-				input_queue1.pop_front()
+			if input_queue.front() < (time_passed + delay):
+				input_queue.pop_front()
 				print("KEY HIT")
 			else:
 				print("BAD INPUT")
 	
 	## ----------------- ANIMALS ----------------
-	if animal_queue1.size() > 0:
-		if animal_queue1.front() < time_passed:
+	if animal_queue.size() > 0:
+		if animal_queue.front() < time_passed:
 			print("BARK")
-			animal_queue1.pop_front()
+			animal_queue.pop_front()
 		pass
 
 ## When song finishes
@@ -47,12 +44,6 @@ func _on_song_length_timeout() -> void:
 	print("SONG DONE")
 
 
-func organise_inputs(complex_arr, animal_number):
-	animal_queue1 = (complex_arr[0])
-	input_queue1 = (complex_arr[1])
-	if animal_number == 2:
-		animal_queue2 = (complex_arr[2])
-		input_queue2 = (complex_arr[3])
-	elif animal_number == 3:
-		animal_queue3 = (complex_arr[4])
-		input_queue3 = (complex_arr[5])
+func organise_inputs(double_arr):
+	animal_queue = (double_arr[0])
+	input_queue = (double_arr[1])
--- gamefile/RhythmTest2/level_editor.gd
+++ gamefile/RhythmTest2/level_editor.gd
@@ -1,27 +1,21 @@
 extends Node2D
 
 ## Level Making, manually change to write Levels
-var editMode = true
+var editMode = false
 ## left bracket is for the animal, right is for the player
 var EDIT_tap_times = [[],[]]
 var current_level_name = "McDInThePentagon_short"
 
 ## !!!FIX!!BUG!!! Name of node wont always be Test1
-@onready var input_handler = get_node("/root/level01")
+@onready var input_handler = get_node("/root/Test1")
 
 ## FOR "tap_times" - left bracket is for the animal, right is for the player
 var levelInfo = {
 	"McDInThePentagon" = {
-		"tap_times": "[[1,2,3,4],[5,6,7,8]]",
-		"animal_amount": 1
+		"tap_times": "[[1,2,3,4],[5,6,7,8]]"
 	},
-	#"McDInThePentagon_short" = {
-		#"tap_times": "[[1.05102644444447, 1.44021611111115, 1.80132722222227, 2.15549388888894, 3.88470622222233, 4.27359511111123, 4.60692844444456, 4.94720622222234], [2.50271611111118, 2.82910500000007, 3.17637288888897, 3.53748400000009, 5.26666666666678, 5.61388888888901, 5.93333333333345, 6.28750000000012]]",
-		#"animal_amount": 1
-	#},
-		"McDInThePentagon_short" = {
-		"tap_times": "[[1,2,3,4],[5,6,7,8]]",
-		"animal_amount": 2
+	"McDInThePentagon_short" = {
+		"tap_times": "[[1.03194444444447, 1.43144434920638, 1.81813055555559, 2.17229722222227, 3.88115222222231, 4.25615222222232, 4.61726400000011, 4.95079574603185], [2.53372563492069, 2.88800088888895, 3.24216755555563, 3.56161200000008, 5.28412907936518, 5.62440685714296, 5.95774019047629, 6.29801796825407]]"
 	},
 }
 
@@ -30,34 +24,17 @@ func _ready() -> void:
 	if !editMode:
 		var tap_times = levelInfo.get(current_level_name).get("tap_times")
 		var tap_times_arr = str_to_var(tap_times)
-		var animal_amount = levelInfo.get(current_level_name).get("animal_amount")
-		input_handler.organise_inputs(tap_times_arr, animal_amount)
+		input_handler.organise_inputs(tap_times_arr)
 
 
 func _process(delta: float) -> void:
 	if editMode:
-		if Input.is_action_just_pressed("EDIT_Bark1"):
-			EDIT_tap_times[0].append(input_handler.time_passed)
-		elif Input.is_action_just_pressed("EDIT_Tap1"):
+		if Input.is_action_just_pressed("tap"):
 			EDIT_tap_times[1].append(input_handler.time_passed)
-		
-		elif Input.is_action_just_pressed("EDIT_Bark2"):
-			## Check if array size exists, then add it
-			if EDIT_tap_times.size() <= 2:
-				EDIT_tap_times.append_array([[],[]])
-			
-			EDIT_tap_times[2].append(input_handler.time_passed)
-		elif Input.is_action_just_pressed("EDIT_Tap2"):
-			EDIT_tap_times[3].append(input_handler.time_passed)
-		
-		elif Input.is_action_just_pressed("EDIT_Bark2"):
-			## Check if array size exists, then add it
-			if EDIT_tap_times.size() <= 4:
-				EDIT_tap_times.append_array([[],[]])
-			
-			EDIT_tap_times[4].append(input_handler.time_passed)
-		elif Input.is_action_just_pressed("EDIT_Tap2"):
-			EDIT_tap_times[5].append(input_handler.time_passed)
+			pass
+		elif Input.is_action_just_pressed("EDIT_Add"):
+			EDIT_tap_times[0].append(input_handler.time_passed)
+			pass
 
 
 func finish():
